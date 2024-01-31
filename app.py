ff67d5f30dca1d399c61d490e0ec4bc4f9d36f74
Anthony Volk <anth.volk@gmail.com>
2024-01-31 19:40:02 +0200

    feat: Add overview of app layout

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+############# DATA FETCHING ##############
+
+# Connect with remote analytics DB
+
+# Fetch all records
+
+# 
+
+############# PAGE STYLING ##############
+
+# Page title
+
+# Chart displaying data visualization,
+# based on selections from below table
+
+# Data selector tool
+
+# Table of filtered queries that's collapsed
+# by default
