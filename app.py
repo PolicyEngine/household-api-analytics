aabd01858f7298ddc69a277f18f61b43973bf64c
Anthony Volk <anth.volk@gmail.com>
2024-01-31 21:07:19 +0200

    feat: Add database connection

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import sqlalchemy
+from google.cloud.sql.connector import Connector, IPTypes
 
 ############# DATA FETCHING ##############
 
 # Connect with remote analytics DB
+
+# Initialize connector object
+connector = Connector()
+
+# Configure connector
+def getconn():
+
+    conn = connector.connect(
+        st.secrets["USER_ANALYTICS_DB_CONNECTION_NAME"],
+        "pymysql",
+        user=st.secrets["USER_ANALYTICS_DB_USERNAME"],
+        password=st.secrets["USER_ANALYTICS_DB_PASSWORD"],
+        db="user_analytics",
+        ip_type=IPTypes.PUBLIC,  # IPTypes.PRIVATE for private IP
+    )
+
+    return conn
+
+db_pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
+)
 
 # Fetch all records
 
